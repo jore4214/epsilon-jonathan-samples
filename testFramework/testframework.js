b5ff686254a3ee046882f57aedce56eafd7ac237
Reuss Jonathan (EXT) <jore4214@eso.local>
2017-11-27 08:44:01 +0100

    testframe continue

--- testFramework/testframework.js
+++ testFramework/testframework.js
@@ -5,20 +5,43 @@ exports.Testframework = class {
     constructor(){
     }
 
+    AssertionException(expected,actual) {
+        this.message = ` expected ${expected} but is ${actual}`;
+        this.name = "AssertionException:";
+    }
+
     assertEquals(expected, actual){
-       if(expected != actual){
-           throw `Testframework.assertEquals: expected ${expected} but is ${actual}`;
+       if(expected !== actual){
+           throw new AssertionException(expected,actual);
        }
     }
 
     runTests(testClass){
         let test = new testClass();
-
+        let testOverview = `Tests of ${testClass}`;
         for(let property in test){
-            //run setup if available
-            if(test.setup){
-                test.setup();
-            }
+
+            if(typeof test[property] === `function` &&  property.startsWith(`test`)){
+
+                //run setup if available
+                try{
+                if(test.setup){
+                    test.setup();
+                }
+                //call test
+
+                test[property]();
+                }catch (e if e instanceof AssertionException){
+                    testOverview += `Test: ${property} failed assertion: ${e.name}${e.message}\n`;
+                    continue;
+                }catch(e){
+                    testOverview += `Test: ${property} throws exception: ${e}\n`;
+                    continue;
+                }
+                testOverview += `Test: ${property}: successful\n`;
+            };
         }
+
+        console.log(testOverview);
     }
 }
